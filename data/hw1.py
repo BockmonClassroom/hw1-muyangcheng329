ff4b6890fa36be5fda64308b47856ee8203c1355
muyangcheng329 <muyangcheng329@gmail.com>
2025-01-17 21:42:30 -0500

    Updated hw1

--- data/hw1.py
+++ data/hw1.py
@@ -5,7 +5,7 @@
 import csv
 
 # define the path
-file_path = "/Users/muyangcheng329/Desktop/Doc/Univ/SPR 2025/DS5110/test/hw1-muyangcheng329/data/Iris.csv"
+file_path = "/Users/muyangcheng329/Desktop/Doc/Univ/SPR 2025/DS5110/hw1-muyangcheng329/data/Iris.csv"
 
 # open the CSV file
 with open(file_path, mode='r') as file:
@@ -13,6 +13,13 @@ with open(file_path, mode='r') as file:
     # read the file
     csv_reader = csv.reader(file)
 
-    # print every line 
+    # line count
+    line_count = 0
+
+     # print every line 
     for line in csv_reader:
         print(line)
+        line_count += 1
+
+    # print the total number of lines
+    print(f"Total lines printed: {line_count}")
